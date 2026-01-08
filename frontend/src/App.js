a2e9a549c6385432ae7fd25a93a8d0d538535009
Sruthi <vindulasruthi20@gmail.com>
2026-01-08 15:45:33 +0530

    Fix broken App.js and remove merge conflict markers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-
-import AskSuggest from "./pages/Ask_Suggest";
-import AskPage from "./pages/AskPage";
-import SuggestPage from "./pages/SuggestPage";
-import ExplorePage from "./pages/ExplorePage";
-import RoadmapPage from "./pages/RoadmapPage";
 import CareerDiscoveryPage from "./pages/CareerDiscoveryPage";
-import DailyTasksPage from "./pages/DailyTasksPage";
-import MentalHealthPage from "./pages/MentalHealthPage";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
-import { MenuContext } from "./context/MenuContext";
 
-/* ---------- Layout ---------- */
 function Layout({ children }) {
   const location = useLocation();
 
   const hideNavbar =
+    location.pathname === "/" ||
     location.pathname === "/login" ||
     location.pathname === "/register";
 
@@ -35,54 +26,36 @@ function Layout({ children }) {
   );
 }
 
-/* ---------- App ---------- */
 export default function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(prev => !prev);
-  };
-
   return (
-    <MenuContext.Provider value={{ menuOpen, toggleMenu }}>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
 
-            {/* PUBLIC */}
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-            {/* PROTECTED */}
-            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="/ask-suggest" element={<ProtectedRoute><AskSuggest /></ProtectedRoute>} />
-            <Route path="/ask" element={<ProtectedRoute><AskPage /></ProtectedRoute>} />
-            <Route path="/suggest" element={<ProtectedRoute><SuggestPage /></ProtectedRoute>} />
-            <Route path="/explore" element={<ProtectedRoute><ExplorePage /></ProtectedRoute>} />
-            <Route
-  path="/roadmap"
-  element={
-    <ProtectedRoute>
-      <RoadmapPage />
-    </ProtectedRoute>
-  }
-/>
-
-            <Route path="/career" element={<ProtectedRoute><CareerDiscoveryPage /></ProtectedRoute>} />
-            <Route path="/tasks" element={<ProtectedRoute><DailyTasksPage /></ProtectedRoute>} />
           <Route
-  path="/wellbeing"
-  element={
-    <ProtectedRoute>
-      <MentalHealthPage />
-    </ProtectedRoute>
-  }
-/>
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </MenuContext.Provider>
+          <Route
+            path="/career"
+            element={
+              <ProtectedRoute>
+                <CareerDiscoveryPage />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
